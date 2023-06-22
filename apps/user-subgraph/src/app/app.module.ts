3059e3c58af4017086fffa9b600a48fe1ad6ff7f
claudealdric <cvaldric@gmail.com>
2023-06-21 23:50:31 -0400

    Use the code-first approach

--- apps/user-subgraph/src/app/app.module.ts
+++ apps/user-subgraph/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql';
 import { Module } from '@nestjs/common';
+import { join } from 'path';
 
 @Module({
   imports: [
-    GraphQLModule.forRoot<ApolloDriverConfig>({ driver: ApolloDriver }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
