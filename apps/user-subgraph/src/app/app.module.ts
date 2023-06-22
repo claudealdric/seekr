77bfead287713f8dfdcd8a14d48b81b238ba5bd1
claudealdric <cvaldric@gmail.com>
2023-06-21 23:48:24 -0400

    Import the GraphQL module

--- apps/user-subgraph/src/app/app.module.ts
+++ apps/user-subgraph/src/app/app.module.ts
@@ -1,10 +1,14 @@
-import { Module } from '@nestjs/common';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { Module } from '@nestjs/common';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({ driver: ApolloDriver }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
