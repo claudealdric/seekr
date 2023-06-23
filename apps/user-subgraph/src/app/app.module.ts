0230dfac10ed093b110d5c55a37038cd2daf7f49
claudealdric <cvaldric@gmail.com>
2023-06-23 00:19:29 -0400

    Add TypeORM and its dependencies

--- apps/user-subgraph/src/app/app.module.ts
+++ apps/user-subgraph/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin
 import { AppController } from './app.controller';
 import { GraphQLModule } from '@nestjs/graphql';
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 
 @Module({
@@ -13,6 +14,16 @@ import { UsersModule } from './users/users.module';
       autoSchemaFile: true,
       playground: false,
       plugins: [ApolloServerPluginLandingPageLocalDefault()],
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'postgres',
+      entities: [],
+      synchronize: true,
     }),
     UsersModule,
   ],
