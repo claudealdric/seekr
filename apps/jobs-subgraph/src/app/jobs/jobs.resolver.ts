14b2b7ed1dfb3ce6ff4b0b70eb0bea708652b947
claudealdric <cvaldric@gmail.com>
2023-06-28 13:48:50 -0500

    Create a `getJobById` query

--- apps/jobs-subgraph/src/app/jobs/jobs.resolver.ts
+++ apps/jobs-subgraph/src/app/jobs/jobs.resolver.ts
@@ -1,5 +1,10 @@
-import { Resolver } from "@nestjs/graphql";
+import { Args, Int, Query, Resolver } from "@nestjs/graphql";
 import { Job } from "./job.object";
 
 @Resolver(() => Job)
-export class JobsResolver {}
+export class JobsResolver {
+	@Query(() => Job)
+	getJobById(@Args("id", { type: () => Int }) id: number) {
+		return { id: 1, title: "Test Title" };
+	}
+}
