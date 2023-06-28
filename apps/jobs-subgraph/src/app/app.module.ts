ffd590539ea1ed53c782e1fae9d028de06f9f68e
claudealdric <cvaldric@gmail.com>
2023-06-28 13:23:33 -0500

    Copy the relevant files from the other subgraph

--- apps/jobs-subgraph/src/app/app.module.ts
+++ apps/jobs-subgraph/src/app/app.module.ts
@@ -1,11 +1,30 @@
+import { ApolloServerPluginLandingPageLocalDefault } from "@apollo/server/plugin/landingPage/default";
+import { ApolloDriver, ApolloDriverConfig } from "@nestjs/apollo";
 import { Module } from "@nestjs/common";
-
+import { GraphQLModule } from "@nestjs/graphql";
+import { TypeOrmModule } from "@nestjs/typeorm";
 import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
 
 @Module({
-	imports: [],
+	imports: [
+		GraphQLModule.forRoot<ApolloDriverConfig>({
+			driver: ApolloDriver,
+			autoSchemaFile: true,
+			playground: false,
+			plugins: [ApolloServerPluginLandingPageLocalDefault()],
+			introspection: true,
+		}),
+		TypeOrmModule.forRoot({
+			type: "postgres",
+			host: process.env.POSTGRES_HOST,
+			port: 5432,
+			username: process.env.POSTGRES_USERNAME,
+			password: process.env.POSTGRES_PASSWORD,
+			database: process.env.POSTGRES_DATABASE,
+			entities: [],
+			synchronize: true,
+		}),
+	],
 	controllers: [AppController],
-	providers: [AppService],
 })
 export class AppModule {}
