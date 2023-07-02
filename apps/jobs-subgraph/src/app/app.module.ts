70412140155d255be2140068f8d8766845026ab5
claudealdric <cvaldric@gmail.com>
2023-07-01 21:11:13 -0500

    Provide the option to override the Postgres port

--- apps/jobs-subgraph/src/app/app.module.ts
+++ apps/jobs-subgraph/src/app/app.module.ts
@@ -23,7 +23,9 @@ import { UsersModule } from "./users/users.module";
 		TypeOrmModule.forRoot({
 			type: "postgres",
 			host: process.env.POSTGRES_HOST,
-			port: 5432,
+			port: process.env.POSTGRES_PORT
+				? Number(process.env.POSTGRES_PORT)
+				: 5432,
 			username: process.env.POSTGRES_USERNAME,
 			password: process.env.POSTGRES_PASSWORD,
 			database: process.env.POSTGRES_DATABASE,
